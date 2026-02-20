3b5b75f9e55521ca17c8de54ffac852187cfed3b
David Philip <pd.philip@gmail.com>
2026-02-20 18:53:35 +0100

    deps upgrades

--- src/Commands/CfRequestCommand.php
+++ src/Commands/CfRequestCommand.php
@@ -3,32 +3,27 @@
 namespace PDPhilip\CfRequest\Commands;
 
 use Illuminate\Console\Command;
+use OmniTerm\HasOmniTerm;
 use PDPhilip\CfRequest\Cloudflare\Cloudflare;
-
-use function OmniTerm\asyncFunction;
-use function OmniTerm\render;
 
 class CfRequestCommand extends Command
 {
+    use HasOmniTerm;
+
     public $signature = 'cf-request:headers';
 
     public $description = 'Uses the Cloudflare API to set the required headers';
 
     public function handle(): int
     {
-
         $hasToken = config('cf-request.cloudflare.token');
 
         if (! $hasToken) {
-
-            render(view('omniterm::status.custom', [
-                'status' => 'error',
-                'name' => 'Config Error',
-                'title' => 'Cloudflare token not found',
-                'help' => [
-                    'Please set the \'token\' value in the config file, or the CF_API_TOKEN environment variable',
-                ],
-            ]));
+            $this->omni->statusError(
+                'Config Error',
+                'Cloudflare token not found',
+                ['Please set the \'token\' value in the config file, or the CF_API_TOKEN environment variable'],
+            );
 
             return self::FAILURE;
         }
@@ -36,58 +31,27 @@ class CfRequestCommand extends Command
         $hasZoneId = config('cf-request.cloudflare.zoneId');
 
         if (! $hasZoneId) {
-
-            render(view('omniterm::status.custom', [
-                'status' => 'error',
-                'name' => 'Config Error',
-                'title' => 'Cloudflare ZoneId not found',
-                'help' => [
-                    'Please set the \'zoneId\' value in the config file, or the CF_API_ZONE_ID environment variable',
-                ],
-            ]));
+            $this->omni->statusError(
+                'Config Error',
+                'Cloudflare ZoneId not found',
+                ['Please set the \'zoneId\' value in the config file, or the CF_API_ZONE_ID environment variable'],
+            );
 
             return self::FAILURE;
         }
 
-        $async = asyncFunction(function () {
+        $result = $this->omni->task('Setting Cloudflare Headers', function () {
             try {
                 $res = Cloudflare::setCfHeaders();
             } catch (\Exception $e) {
-                return ['success' => false, 'message' => $e->getMessage()];
+                return ['state' => 'error', 'message' => $e->getMessage()];
             }
 
-            return $res;
-
+            return [
+                'state' => $res['success'] ? 'success' : 'error',
+                'message' => $res['message'],
+            ];
         });
-        $async->withFailOver(view('omniterm::loaders.spinner', [
-            'state' => 'failover',
-            'message' => 'Setting Cloudflare Headers',
-            'i' => 0,
-        ]));
-        $result = $async->run(function () use ($async) {
-            $async->render(view('omniterm::loaders.spinner', [
-                'colors' => ['text-amber-500', 'text-emerald-500', 'text-rose-500', 'text-sky-500'],
-                'state' => 'running',
-                'message' => 'Setting Cloudflare Headers',
-                'i' => $async->getInterval(),
-            ]));
-        });
-
-        if ($result['success']) {
-            $async->render(view('omniterm::loaders.spinner', [
-                'state' => 'success',
-                'message' => 'Cloudflare Headers Set',
-                'details' => $result['message'],
-                'i' => $async->getInterval(),
-            ]));
-        } else {
-            $async->render(view('omniterm::loaders.spinner', [
-                'state' => 'error',
-                'message' => 'Error',
-                'details' => $result['message'],
-                'i' => $async->getInterval(),
-            ]));
-        }
 
         return self::SUCCESS;
     }
